43f6247b21f7c83b928b4b6396af3ade75918b17
wesleymutwiri <wesleymochiemo@gmail.com>
2018-05-05 00:00:11 +0300

    added classmethod for location model

--- g2/models.py
+++ g2/models.py
@@ -3,12 +3,30 @@ import datetime as dt
 # Create your models here.
 class Category(models.Model):
     category = models.CharField(max_length=50)
+    
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
 
     def __str__(self):
         return self.category
 
 class Location(models.Model):
     location = models.CharField(max_length=50)
+    
+    def save_location(self):
+        self.save()
+    
+    def delete_location(self):
+        self.delete()   
+    
+    @classmethod
+    def update_location(cls, id, location, update):
+        updated = cls.objects.filter(id=id).update(location=update)
+        return updated
+    
     def __str__(self):
         return self.location
 
@@ -17,9 +35,17 @@ class Images(models.Model):
     image_url = models.TextField(blank=True)
     image_name = models.CharField(max_length=30, blank=True)
     image_description = models.CharField(max_length=120,blank= True)
-    display_date = models.DateTimeField(auto_now_add=True)
     category = models.ManyToManyField(Category,blank=True)
+    post_date = models.DateTimeField(auto_now=True)
     location = models.ForeignKey('Location')
 
+    class Meta:
+        ordering = ['-post_date']   
     
+    def save_image(self):
+        self.save()
+    
+    def delete_image(self):
+        self.delete() 
+
 
--- gallery2/settings.py
+++ gallery2/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY=config('SECRET_KEY')
 DEBUG=config('DEBUG',default=False,cast=bool)
 
 ALLOWED_HOSTS = ['*']
+TIME_ZONE = 'Africa/Nairobi'
 
 
 # Application definition

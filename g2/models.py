c9f544df88117e14deb2bdc5518e96906f1a72a7
wesleymutwiri <wesleymochiemo@gmail.com>
2018-05-04 23:19:41 +0300

    new models

--- g2/models.py
+++ g2/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+import datetime as dt
 # Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.category
+
+class Location(models.Model):
+    location = models.CharField(max_length=50)
+    def __str__(self):
+        return self.location
+
+class Images(models.Model):
+    image = models.ImageField(upload_to = 'articles/', blank=True)
+    image_url = models.TextField(blank=True)
+    image_name = models.CharField(max_length=30, blank=True)
+    image_description = models.CharField(max_length=120,blank= True)
+    display_date = models.DateTimeField(auto_now_add=True)
+    category = models.ManyToManyField(Category,blank=True)
+    location = models.ForeignKey('Location')
+
+    
+

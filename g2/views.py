8b81d1f10508820b2254e260be946b81a7a75198
wesleymutwiri <wesleymochiemo@gmail.com>
2018-05-06 00:37:00 +0300

    added search function

--- g2/views.py
+++ g2/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Images, Category, Location
 import datetime as dt
+
 # Create your views here.
+
 def index(request):
     date=dt.date.today
     image = Images.get_all_images()
@@ -10,3 +12,16 @@ def index(request):
 def present(request, image_id):
     image = Images.get_image_by_id(image_id)
     return render(request, 'g2/present.html', {"image":image})
+
+def search_images(request):
+    if 'image' in request.GET and request.GET["image"]:
+        write = request.GET.get("image")
+        found = Images.search_by_image(write)
+        message = f"{write}"
+        return render(request,'search.html',{"message":message,"found":found})
+    
+    else:
+        message = "No search image found within the database"
+        return render(request,'search.html',{"message":message})
+
+        

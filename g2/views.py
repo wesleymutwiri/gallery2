70a734b3c8fb0536d87acfce642e4d2fe2ce0c5e
wesleymutwiri <wesleymochiemo@gmail.com>
2018-05-06 14:38:46 +0300

    progress in styling

--- g2/views.py
+++ g2/views.py
@@ -21,7 +21,21 @@ def search_images(request):
         return render(request,'g2/search.html',{"message":message,"found":found})
     
     else:
-        message = "No search image found within the database"
+        message = "No such image found within the database"
         return render(request,'g2/search.html',{"message":message})
 
+def search_locations(request):
+    if image in request.GET and request.GET["image"]:
+        write = request.GET.get("image")
+        found = Images.get_image_by_location(write)
+        message = f"{write}"
+        return render(request, 'g2/location.html',{"message":message,"found":found})
+
+    else:
+        message = "No such location found in the database "
+        return render(request,'g2/location.html',{"message":message})
+
+def view_information(request):
+    view = Images.get_all_images()
+    return render(request, 'g2/images.html',{"date":date "view":view })
         

6d92ce8d25f135c26c967f281df03354036db9cf
wesleymutwiri <wesleymochiemo@gmail.com>
2018-05-07 11:32:15 +0300

    deploy attempt 482

--- gallery2/settings.py
+++ gallery2/settings.py
@@ -133,5 +133,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
